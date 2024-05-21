2bba59c574df18992c970f418a7e4579d8105959
Avior Kahalani <aviorkahalani@gmail.com>
2024-05-22 02:06:58 +0300

    feat: remove redundant lines

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { Outlet } from 'react-router-dom'
-import { useFetchCatBreedsQuery } from './store'
 
 function App() {
-  const { data, isLoading, error } = useFetchCatBreedsQuery()
-  console.log('data:', data)
-
   return (
     <>
       <Outlet />
