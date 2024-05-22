82f51e0cd49f475a2c6317c4a6e8e0265ac58eed
Avior Kahalani <aviorkahalani@gmail.com>
2024-05-22 17:10:24 +0300

    feat: build config

--- src/store/cat.ts
+++ src/store/cat.ts
@@ -1,9 +1,14 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import type { CatBreed } from '../types'
 
+const BASE_URL =
+  process.env.NODE_ENV !== 'development'
+    ? '/api/cat'
+    : '//localhost:3030/api/cat'
+
 export const catApi = createApi({
   reducerPath: 'catApi',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3030/api/cat' }),
+  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
   endpoints: (builder) => ({
     fetchCatBreeds: builder.query<CatBreed[], void>({
       query: () => '',
