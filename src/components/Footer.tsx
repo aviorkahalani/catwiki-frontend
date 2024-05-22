5122b5093ef5baa24dbee98fc46fc1d10dfab1f1
Avior Kahalani <aviorkahalani@gmail.com>
2024-05-22 03:07:33 +0300

    feat: add footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import { Link } from 'react-router-dom'
+import Container from './Container'
+import { Cat } from '@phosphor-icons/react'
+
+export default function Footer() {
+  return (
+    <footer className="pt-4">
+      <Container className="flex items-center justify-between rounded-t-lg bg-gray-800 p-5 text-white">
+        <Link to="/" className="flex items-center gap-2.5 font-mono text-xl">
+          CatWiki <Cat size={20} />
+        </Link>
+        <p className="text-sm font-light opacity-70">
+          ©️ Created By Avior Kahalani - 2024
+        </p>
+      </Container>
+    </footer>
+  )
+}
