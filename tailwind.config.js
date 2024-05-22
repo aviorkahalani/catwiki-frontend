b2bbfcb65000e193f3b188efac08f405dc8a9d65
Avior Kahalani <aviorkahalani@gmail.com>
2024-05-22 17:48:13 +0300

    fix: change hero bg url

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,10 @@
-const imageUrl = new URL('./src/assets/cat-hero.webp', import.meta.url).href
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
       backgroundImage: {
-        hero: `url(${imageUrl})`,
+        hero: `url('https://res.cloudinary.com/avior-projects/image/upload/v1716389234/cat-hero_wzamwe.webp')`,
       },
     },
   },
